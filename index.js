eed0e7a30cfc722b40dd58fde9f6a71551e95f3a
Ben Holmes <mail@benholmes.uk>
2015-03-17 19:36:57 -0700

    Update addon export format

--- index.js
+++ index.js
@@ -6,44 +6,41 @@ var fs = require('fs');
 var pickFiles = require('broccoli-static-compiler');
 var mergeTrees = require('broccoli-merge-trees');
 
-function unwatchedTree(dir) {
-  return {
-    read:    function() { return dir; },
-    cleanup: function() { }
-  };
-}
+module.exports = {
+  name: 'ember-cli-mapbox',
 
-function EmberCLIMapbox(project) {
-  this.project = project;
-  this.name = 'Mapbox for Ember CLI';
-}
+  unwatchedTree: function(dir) {
+    return {
+      read:    function() { return dir; },
+      cleanup: function() { }
+    };
+  },
 
-EmberCLIMapbox.prototype.treeFor = function treeFor(name) {
-  var treePath =  path.join('node_modules', 'ember-cli-mapbox', name);
+  treeFor: function(name) {
+    var treePath =  path.join('node_modules', 'ember-cli-mapbox', name);
 
-  if (fs.existsSync(treePath)) {
-    return unwatchedTree(treePath);
-  }
+    if (fs.existsSync(treePath)) {
+      return this.unwatchedTree(treePath);
+    }
+  },
+
+  included: function(app) {
+    app.import('vendor/mapbox.js/dist/mapbox.uncompressed.js');
+    app.import('vendor/mapbox.js/dist/mapbox.uncompressed.css');
+  },
+
+  postprocessTree: function(type, workingTree) {
+    if (type === 'all') {
+      var treePath = path.join('node_modules', 'ember-cli-mapbox', 'vendor', 'mapbox.js', 'dist');
+      var staticFiles = pickFiles(this.unwatchedTree(treePath), {
+        srcDir: 'images',
+        files: ['icons.svg', '*.png', 'images/*.png'],
+        destDir: '/assets/images'
+      });
+
+      return mergeTrees([workingTree, staticFiles]);
+    } else {
+      return workingTree;
+    }
+  },
 };
-
-EmberCLIMapbox.prototype.included = function included(app) {
-  app.import('vendor/mapbox.js/dist/mapbox.uncompressed.js');
-  app.import('vendor/mapbox.js/dist/mapbox.uncompressed.css');
-};
-
-EmberCLIMapbox.prototype.postprocessTree = function postprocessTree(type, workingTree) {
-  if (type === 'all') {
-    var treePath = path.join('node_modules', 'ember-cli-mapbox', 'vendor', 'mapbox.js', 'dist');
-    var staticFiles = pickFiles(unwatchedTree(treePath), {
-      srcDir: 'images',
-      files: ['icons.svg', '*.png', 'images/*.png'],
-      destDir: '/assets/images'
-    });
-
-    return mergeTrees([workingTree, staticFiles]);
-  } else {
-    return workingTree;
-  }
-};
-
-module.exports = EmberCLIMapbox;
